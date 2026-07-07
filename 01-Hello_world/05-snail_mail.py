f810b6ae5ca69cf119993955bcb2e4f8181e589a
PranithaReddy22 <192425044.simats@saveetha.com>
2026-07-07 17:44:43 +0530

    Add snail_mail.py  future self letter exercise

---
+++ 01-Hello_world/05-snail_mail.py
@@ -0,0 +1,4 @@
+print("07-07-2026")
+print("right now i am feeling some good because i am trying new things and i really dont know whether it works well or not,yeah i am trusting the process.")
+print("i wanna learn coding nd i wanna try to get a job in my dream company")
+print("hey i hope u have become an programmer with all your hardwork and consistency and Congrats!")
